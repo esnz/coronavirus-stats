aae477d507e856bc8503ca16a2c6733ebbb073bb
Ehsan <Ehsan@EHSANPC>
2020-04-13 12:12:52 +0430

    added new api endpoint

--- src/api.js
+++ src/api.js
@@ -2,3 +2,8 @@ export async function getSummaryStats() {
     const data = await fetch('https://api.covid19api.com/summary');
     return data.json();
 }
+
+export async function getDayOneDataByCountry(slug, status) {
+    const data = await fetch(`https://api.covid19api.com/total/dayone/country/${slug}/status/${status}`);
+    return data.json();
+}
